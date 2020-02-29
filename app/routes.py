49188c36c1ef1e908816b9a4ea7950220df38bd0
Samuel <samuel.wyatt@ttu.edu>
2020-02-29 14:29:56 -0700

    commit working test

--- app/routes.py
+++ app/routes.py
@@ -3,22 +3,71 @@ from flask import Flask, request, jsonify, render_template, redirect, flash, url
 from flask_login import current_user, login_user, logout_user, login_required
 import base64
 from urllib.parse import unquote_plus
+import avro.schema
+from avro.datafile import DataFileReader, DataFileWriter
+from avro.io import DatumReader, DatumWriter
+import io
+import os
+import binascii
+from PIL import Image
+
+UPLOAD_DIRECTORY = os.getcwd()+'/app/static'
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    upload_avros = [x for x in os.listdir(UPLOAD_DIRECTORY) if '.avro' in x]
+    return render_template('index.html', uploads=upload_avros)
 
+@app.route('/upload')
+def target():
 
+    args = request.args
+    avrofile = args.get('file')
+
+    targets = DataFileReader(open(UPLOAD_DIRECTORY+'/'+avrofile, "rb"), DatumReader())
+    
+    tts = [{
+        'ObjectID':x['ObjectID'],
+        'RA': x['RA'],
+        'DEC': x['DEC'],
+        'RAPM': x['RAPM'],
+        'DECPM': x['DECPM'],
+        'MAG': x['MAG'],
+        'EQUINOX': x['EQUINOX'],
+        'FinderFile': str(x['FinderFile'])
+    } for x in targets]
+
+    return render_template('upload.html', targets=tts)
+    
 
 @app.route('/target_upload', methods=['POST'])
 def target_upload():
-	print('test')
-	args = request.args
-	url_unquote = unquote_plus(args['file'])
-	print(url_unquote)
-	compress_file = str.encode(url_unquote)
-	print(compress_file)
-	decompress = base64.b64decode(compress_file)
-	return(jsonify(decompress))
-	#return(jsonify(args))
+
+    json_args = request.get_json()
+
+    avro_file = base64.b64decode(str.encode(unquote_plus(json_args['file'])))
+    of = io.BytesIO(avro_file) 
+
+    targets = DataFileReader(of, DatumReader())
+    
+    schema = avro.schema.parse(open('mmt_target.avsc', 'rb').read())
+    writer = DataFileWriter(open(UPLOAD_DIRECTORY+'/test_avro.avro', 'wb'), DatumWriter(), schema)
+    for t in targets:
+        writer.append(t)
+    writer.close()
+
+    tts = DataFileReader(open(UPLOAD_DIRECTORY+'/test_avro.avro', 'rb'), DatumReader())
+    for t in tts:
+        print(t)
+
+    finder_bytes = base64.b64decode(str.encode(unquote_plus(json_args['finder'])))
+    stream = io.BytesIO(finder_bytes)
+    img = Image.open(stream)
+    img.save(UPLOAD_DIRECTORY+"/"+json_args['finder_filename'])
+
+    return(jsonify('success'))
+
+@app.route('/targets', methods=['GET'])
+def targets():
+    pass
--- mmt_avro.py
+++ mmt_avro.py
@@ -1,19 +1,44 @@
 import avro.schema
 from avro.datafile import DataFileReader, DataFileWriter
 from avro.io import DatumReader, DatumWriter
-
+import base64
+from urllib.parse import quote_plus
+import requests
+import imageio
 
 def main():
-	schema = avro.schema.parse(open("mmt_avro2.avsc", "rb").read())
-	writer = DataFileWriter(open("targets.avro", "wb"), DatumWriter(), schema)
-	writer.append(
-		{
-			"ObjectID": "m31", 
-		})
-	writer.close()
 
-	targets = DataFileReader(open("targets.avro", "rb"), DatumReader())
-	for t in targets:
-		print(t)
+    finder_file = 'm31.jpg'
+    target_file = 'm31.avro'
+    schema = avro.schema.parse(open("mmt_target.avsc", "rb").read())
+    writer = DataFileWriter(open(target_file, "wb"), DatumWriter(), schema)
+    writer.append({
+        "ObjectID": "m3100",
+        "RA":42,
+        "DEC":42,
+        "RAPM":0.0,
+        "DECPM":0.0,
+        "MAG":4.3,
+        "PA":180.0,
+        "EQUINOX":"J2000",
+        "FinderFile":finder_file
+    })
+    writer.close()
+
+    with open(target_file, 'rb') as tf:
+        contents = tf.read()
+
+    with open(finder_file, 'rb') as ff:
+        finder = ff.read()
+
+    json = {
+                "file": quote_plus(base64.b64encode(contents)),
+                "finder":quote_plus(base64.b64encode(finder)),
+                "fmt":"jpg",
+                "finder_filename":finder_file
+            }
+
+    r = requests.post('http://127.0.0.1:5000/target_upload', json=json)
+    print(r.text)
 
 main()
